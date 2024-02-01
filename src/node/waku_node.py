4cf303cdf66e5fa6ce65f80fe710ba2b63766026
Roman <zajic@zajic.net>
2024-02-01 15:08:19 +0800

    fix: use absolute path for the volumes names - mark volumes shared across containers

--- src/node/waku_node.py
+++ src/node/waku_node.py
@@ -78,7 +78,7 @@ class WakuNode:
             default_args[key] = value
 
         if len(default_args["rln-creds"]) == 4:
-            self._volumes.extend(["./rln_tree:/etc/rln_tree/:Z", "./keystore/keystore.json:/keystore/keystore.json/:Z"])
+            self._volumes.extend(["/rln_tree:/etc/rln_tree/:z", "/keystore/keystore.json:/keystore/keystore.json/:z"])
             rln_opts = {}
             if self.is_gowaku():
                 rln_opts = {
