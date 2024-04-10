d34a2ff24f2b40dcae460da92d44092f9e93a863
Florin Barbu <barbu_florin_adrian@yahoo.com>
2024-04-10 11:46:59 +0300

    new test

--- tests/light_push/test_multiple_nodes.py
+++ tests/light_push/test_multiple_nodes.py
@@ -37,6 +37,19 @@ class TestMultipleNodes(StepsLightPush):
         self.check_light_pushed_message_reaches_receiving_peer(sender=self.light_push_node1)
         self.check_light_pushed_message_reaches_receiving_peer(sender=self.light_push_node2)
 
+    def test_combination_of_different_nodes(self):
+        self.setup_first_receiving_node(lightpush="true", relay="true", filter="true")
+        self.setup_second_receiving_node(lightpush="false", relay="false", filternode=self.receiving_node1.get_multiaddr_with_id())
+        self.setup_first_lightpush_node(lightpush="true", relay="false")
+        self.setup_second_lightpush_node(lightpush="true", relay="true")
+        self.subscribe_to_pubsub_topics_via_relay(node=self.receiving_node1)
+        self.subscribe_to_pubsub_topics_via_relay(node=self.light_push_node2)
+        self.subscribe_to_pubsub_topics_via_filter(node=self.receiving_node2)
+        self.check_light_pushed_message_reaches_receiving_peer(sender=self.light_push_node1)
+        self.check_light_pushed_message_reaches_receiving_peer(sender=self.light_push_node2)
+        get_messages_response = self.receiving_node2.get_filter_messages(self.test_content_topic)
+        assert len(get_messages_response) == 2, "Lightpushed message was not relayed to the filter node"
+
     def test_multiple_receiving_nodes(self):
         self.setup_first_receiving_node(lightpush="true", relay="true")
         self.setup_additional_receiving_nodes()
