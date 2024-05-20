a3de9b954750ab35017b864d9aa15f83fd5537c5
Florin Barbu <barbu_florin_adrian@yahoo.com>
2024-05-20 11:40:43 +0300

    fixes

--- tests/store/test_reliability.py
+++ tests/store/test_reliability.py
@@ -1,3 +1,4 @@
+import pytest
 from src.libs.custom_logger import get_custom_logger
 from src.libs.common import delay
 from src.steps.store import StepsStore
@@ -5,15 +6,16 @@ from src.steps.store import StepsStore
 logger = get_custom_logger(__name__)
 
 
+@pytest.mark.usefixtures("node_setup")
 class TestReliability(StepsStore):
-    def test_publishing_node_is_stopped(self, node_setup):
+    def test_publishing_node_is_stopped(self):
         self.publish_message()
         self.check_published_message_is_stored(page_size=5, ascending="true")
         self.publishing_node1.stop()
         store_response = self.store_node1.get_store_messages(pubsub_topic=self.test_pubsub_topic, page_size=5, ascending="true", store_v="v3")
         assert len(store_response["messages"]) == 1
 
-    def test_publishing_node_restarts(self, node_setup):
+    def test_publishing_node_restarts(self):
         self.publish_message()
         self.check_published_message_is_stored(page_size=5, ascending="true")
         self.publishing_node1.restart()
@@ -26,7 +28,7 @@ class TestReliability(StepsStore):
             store_response = node.get_store_messages(pubsub_topic=self.test_pubsub_topic, page_size=5, ascending="true", store_v="v3")
             assert len(store_response["messages"]) == 2
 
-    def test_store_node_restarts(self, node_setup):
+    def test_store_node_restarts(self):
         self.publish_message()
         self.check_published_message_is_stored(page_size=5, ascending="true")
         self.store_node1.restart()
@@ -38,7 +40,7 @@ class TestReliability(StepsStore):
             store_response = node.get_store_messages(pubsub_topic=self.test_pubsub_topic, page_size=5, ascending="true", store_v="v3")
             assert len(store_response["messages"]) == 2
 
-    def test_publishing_node_paused_and_unpaused(self, node_setup):
+    def test_publishing_node_paused_and_unpaused(self):
         self.publish_message()
         self.check_published_message_is_stored(page_size=5, ascending="true")
         self.publishing_node1.pause()
@@ -51,7 +53,7 @@ class TestReliability(StepsStore):
             store_response = node.get_store_messages(pubsub_topic=self.test_pubsub_topic, page_size=5, ascending="true", store_v="v3")
             assert len(store_response["messages"]) == 2
 
-    def test_store_node_paused_and_unpaused(self, node_setup):
+    def test_store_node_paused_and_unpaused(self):
         self.publish_message()
         self.check_published_message_is_stored(page_size=5, ascending="true")
         self.store_node1.pause()
@@ -64,7 +66,7 @@ class TestReliability(StepsStore):
             store_response = node.get_store_messages(pubsub_topic=self.test_pubsub_topic, page_size=5, ascending="true", store_v="v3")
             assert len(store_response["messages"]) == 2
 
-    def test_message_relayed_while_store_node_is_paused(self, node_setup):
+    def test_message_relayed_while_store_node_is_paused(self):
         self.publish_message()
         self.check_published_message_is_stored(page_size=5, ascending="true")
         self.store_node1.pause()
@@ -76,7 +78,7 @@ class TestReliability(StepsStore):
             store_response = node.get_store_messages(pubsub_topic=self.test_pubsub_topic, page_size=5, ascending="true", store_v="v3")
             assert len(store_response["messages"]) == 2
 
-    def test_message_relayed_while_store_node_is_stopped(self, node_setup):
+    def test_message_relayed_while_store_node_is_stopped(self):
         self.publish_message()
         self.check_published_message_is_stored(page_size=5, ascending="true")
         self.store_node1.stop()
@@ -86,18 +88,16 @@ class TestReliability(StepsStore):
         self.add_node_peer(self.store_node1, self.multiaddr_list)
         self.subscribe_to_pubsub_topics_via_relay(node=self.store_node1)
         delay(1)
-        self.check_published_message_is_stored(page_size=5, ascending="true")
         for node in self.store_nodes:
             store_response = node.get_store_messages(pubsub_topic=self.test_pubsub_topic, page_size=5, ascending="true", store_v="v3")
             assert len(store_response["messages"]) == 2
 
     def test_message_relayed_before_store_node_is_started(self):
-        self.setup_first_publishing_node(store="true", relay="true")
-        self.subscribe_to_pubsub_topics_via_relay()
         self.publish_message()
-        self.setup_first_store_node(store="true", relay="true")
-        delay(10)
+        self.check_published_message_is_stored(page_size=5, ascending="true")
+        self.setup_second_store_node(store="true", relay="true")
         self.subscribe_to_pubsub_topics_via_relay()
-        self.add_node_peer(self.store_node1, self.multiaddr_list)
-        delay(10)
+        store_response = self.store_node2.get_store_messages(pubsub_topic=self.test_pubsub_topic, page_size=5, ascending="true", store_v="v3")
+        assert len(store_response["messages"]) == 0
+        self.publish_message()
         self.check_published_message_is_stored(page_size=5, ascending="true")
