ac74f57c096ca47689f3aa1907a03e39609237a0
Roman <zajic@zajic.net>
2024-06-21 16:58:31 +0200

    fix: skipp all RLN tests till registration works

--- tests/relay/test_rln.py
+++ tests/relay/test_rln.py
@@ -16,8 +16,8 @@ logger = get_custom_logger(__name__)
 @pytest.mark.xdist_group(name="RLN serial tests")
 @pytest.mark.usefixtures("register_main_rln_relay_nodes")
 @pytest.mark.skipif("go-waku" in (NODE_1 + NODE_2), reason="Test works only with nwaku")
+@pytest.mark.skip(reason="waiting to resolve registration https://github.com/waku-org/nwaku/issues/2837")
 class TestRelayRLN(StepsRLN, StepsRelay):
-    @pytest.mark.timeout(600)
     def test_valid_payloads_at_slow_rate(self):
         self.setup_main_rln_relay_nodes()
         self.subscribe_main_relay_nodes()
@@ -33,7 +33,6 @@ class TestRelayRLN(StepsRLN, StepsRelay):
             delay(1)
             assert not failed_payloads, f"Payloads failed: {failed_payloads}"
 
-    @pytest.mark.timeout(600)
     def test_valid_payloads_at_spam_rate(self):
         self.setup_main_rln_relay_nodes()
         self.subscribe_main_relay_nodes()
