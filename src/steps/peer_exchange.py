a8d5539b87c0ec33c9b825f8ceaacbd1aa446fd9
Roman <zajic@zajic.net>
2024-07-04 17:05:02 +0200

    test: get peers for filter node

--- src/steps/peer_exchange.py
+++ src/steps/peer_exchange.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 from src.env_vars import NODE_2
 from src.node.waku_node import WakuNode
 
@@ -10,7 +12,7 @@ class StepsPeerExchange(StepsRelay):
     responder_multiaddr = ""
 
     @allure.step
-    def setup_peer_exchange_requester_node(self, **kwargs):
+    def setup_third_node_as_peer_exchange_requester(self, **kwargs):
         self.node3 = WakuNode(NODE_2, f"node3_{self.test_id}")
         self.node3.start(
             relay="true",
@@ -19,3 +21,10 @@ class StepsPeerExchange(StepsRelay):
         )
         self.add_node_peer(self.node3, [self.multiaddr_with_id])
         self.main_nodes.extend([self.node3])
+
+    @allure.step
+    def setup_fourth_node_as_filter(self, **kwargs):
+        self.node4 = WakuNode(NODE_2, f"node4_{self.test_id}")
+        self.node4.start(relay="false", **kwargs)
+        self.add_node_peer(self.node4, [self.multiaddr_with_id])
+        self.main_nodes.extend([self.node4])
--- tests/peer_exchange/test_peer_exchange.py
+++ tests/peer_exchange/test_peer_exchange.py
@@ -11,11 +11,9 @@ class TestPeerExchange(StepsPeerExchange):
         node1_peers = self.node1.get_peers()
         assert len(node1_peers) == 1
         self.responder_multiaddr = peer_info2multiaddr(node1_peers[0], self.node1.is_nwaku())
-        self.setup_peer_exchange_requester_node(discv5_discovery="false")
+        self.setup_third_node_as_peer_exchange_requester(discv5_discovery="false")
 
-        node1_id = self.node1.get_id()
-        node2_id = self.node2.get_id()
-        others = {node1_id, node2_id}
+        others = {self.node1.get_id(), self.node2.get_id()}
 
         own = set()
         for peer_info in self.node3.get_peers():
@@ -23,3 +21,21 @@ class TestPeerExchange(StepsPeerExchange):
             own.add(peer_id)
 
         assert own == others, f"Not all nodes found as expected in peer store of Node3"
+
+    def test_get_peers_for_filter_node(self):
+        self.setup_first_relay_node(filter="true", relay_peer_exchange="true")
+        self.setup_second_relay_node(filter="true", peer_exchange="true")
+        delay(1)
+        node1_peers = self.node1.get_peers()
+        assert len(node1_peers) == 1
+        self.responder_multiaddr = peer_info2multiaddr(node1_peers[0], self.node1.is_nwaku())
+        self.setup_third_node_as_peer_exchange_requester(discv5_discovery="false")
+
+        suitable_peers = []
+        for peer_info in self.node3.get_peers():
+            multiaddr = peer_info2multiaddr(peer_info, self.node3.is_nwaku())
+            suitable_peers.append(multiaddr)
+
+        assert len(suitable_peers) == 2
+
+        self.setup_fourth_node_as_filter(filternode=suitable_peers[0])
