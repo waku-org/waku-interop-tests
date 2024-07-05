285b2b2e96cd978efc5aa100d8e3f289cd274efe
Roman <zajic@zajic.net>
2024-07-05 13:22:15 +0200

    test: get peers after node1 was restarted

--- tests/peer_exchange/test_peer_exchange.py
+++ tests/peer_exchange/test_peer_exchange.py
@@ -39,3 +39,27 @@ class TestPeerExchange(StepsPeerExchange):
         assert len(suitable_peers) == 2
 
         self.setup_fourth_node_as_filter(filternode=suitable_peers[0])
+
+    def test_get_peers_after_node1_was_restarted(self):
+        self.setup_first_relay_node(relay_peer_exchange="true")
+        self.setup_second_relay_node(peer_exchange="true")
+        delay(1)
+        node1_peers = self.node1.get_peers()
+        assert len(node1_peers) == 1
+        self.responder_multiaddr = peer_info2multiaddr(node1_peers[0], self.node1.is_nwaku())
+
+        self.node1.restart()
+
+        # Give Node2 time to update its peer store
+        delay(1)
+
+        # Start Node3
+        self.setup_third_node_as_peer_exchange_requester(discv5_discovery="false")
+        others = {self.node1.get_id(), self.node2.get_id()}
+
+        own = set()
+        for peer_info in self.node3.get_peers():
+            peer_id = multiaddr2id(peer_info2multiaddr(peer_info, self.node3.is_nwaku()))
+            own.add(peer_id)
+
+        assert own == others, f"Two nodes are expected in peer store of Node3"
