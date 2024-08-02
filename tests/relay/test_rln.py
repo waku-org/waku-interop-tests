a396d00dbde44116ac57ab951363283a36d08b2a
Roman <zajic@zajic.net>
2024-08-02 11:24:38 +0800

    fix: on-chain tests

--- tests/relay/test_rln.py
+++ tests/relay/test_rln.py
@@ -123,7 +123,7 @@ class TestRelayRLN(StepsRLN, StepsRelay):
             message = self.create_message(payload=to_base64(payload["value"]))
             try:
                 logger.debug(f"Sending message No. #{i + 1}")
-                self.check_published_message_reaches_relay_peer(message, message_propagation_delay=0.2)
+                self.check_published_message_reaches_relay_peer(message, message_propagation_delay=0.1)
             except Exception as e:
                 logger.error(f'Payload {payload["description"]} failed: {str(e)}')
                 failed_payloads.append(payload["description"])
@@ -134,23 +134,22 @@ class TestRelayRLN(StepsRLN, StepsRelay):
 
     @pytest.mark.timeout(600)
     def test_valid_payloads_dynamic_at_spam_rate(self):
-        message_limit = 20
+        message_limit = 100
         epoch_sec = 600
         self.setup_main_rln_relay_nodes(
             rln_relay_user_message_limit=message_limit, rln_relay_epoch_sec=epoch_sec, rln_relay_dynamic="true", wait_for_node_sec=600
         )
         self.subscribe_main_relay_nodes()
-        previous = math.trunc(time())
+        start = math.trunc(time())
         for i, payload in enumerate(self.SAMPLE_INPUTS_RLN):
             logger.debug(f'Running test with payload {payload["description"]}')
             message = self.create_message(payload=to_base64(payload["value"]))
             try:
+                logger.debug(f"Sending message No. #{i + 1}")
                 now = math.trunc(time())
                 self.publish_message(message)
-                if i > 0 and (now - previous) == 0:
+                if i > message_limit and (now - start) <= epoch_sec:
                     raise AssertionError("Publish with RLN enabled at spam rate worked!!!")
-                else:
-                    previous = now
             except Exception as e:
                 assert "RLN validation failed" or "NonceLimitReached" in str(e)
 
