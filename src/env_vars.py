ab831063d0d1bdb64dbd8050c655d4b60412fc72
Roman <zajic@zajic.net>
2024-08-09 15:05:06 +0800

    fix: delete test workflows - revert env_vars

--- src/env_vars.py
+++ src/env_vars.py
@@ -16,9 +16,9 @@ def get_env_var(var_name, default=None):
 # Configuration constants. Need to be upercase to appear in reports
 DEFAULT_NWAKU = "wakuorg/nwaku:latest"
 DEFAULT_GOWAKU = "wakuorg/go-waku:latest"
-NODE_1 = get_env_var("NODE_1", DEFAULT_NWAKU)
+NODE_1 = get_env_var("NODE_1", DEFAULT_GOWAKU)
 NODE_2 = get_env_var("NODE_2", DEFAULT_NWAKU)
-ADDITIONAL_NODES = get_env_var("ADDITIONAL_NODES", f"{DEFAULT_NWAKU},{DEFAULT_NWAKU},{DEFAULT_NWAKU}")
+ADDITIONAL_NODES = get_env_var("ADDITIONAL_NODES", f"{DEFAULT_NWAKU},{DEFAULT_GOWAKU},{DEFAULT_NWAKU}")
 # more nodes need to follow the NODE_X pattern
 DOCKER_LOG_DIR = get_env_var("DOCKER_LOG_DIR", "./log/docker")
 NETWORK_NAME = get_env_var("NETWORK_NAME", "waku")
