d2348c8507656df3d9b5f94046dc6e1a888ddece
aya <ayahassan2877@gmail.com>
2024-11-07 10:40:51 +0200

    increase nodes to 15 in test_filter_many_subscribed_nodes

--- tests/e2e/test_e2e.py
+++ tests/e2e/test_e2e.py
@@ -222,7 +222,7 @@ class TestE2E(StepsFilter, StepsStore, StepsRelay, StepsLightPush):
 
     @pytest.mark.timeout(60 * 7)
     def test_filter_3_senders_multiple_msg_1_receiver(self):
-        messages_num = 3
+        messages_num = 10
         total_senders = 3
         self.node4 = WakuNode(NODE_1, f"node4_{self.test_id}")
         self.node5 = WakuNode(NODE_1, f"node5_{self.test_id}")
@@ -258,7 +258,7 @@ class TestE2E(StepsFilter, StepsStore, StepsRelay, StepsLightPush):
 
     @pytest.mark.timeout(60 * 5)
     def test_filter_many_subscribed_nodes(self):
-        max_subscribed_nodes = 2
+        max_subscribed_nodes = 15
         if STRESS_ENABLED:
             max_subscribed_nodes = 50
         node_list = []
